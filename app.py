5c147ee39dd21a85350e71341324c467a7768f52
shashijangra <shashijangra@gmail.com>
2019-02-11 21:32:35 +0530

    updated ip

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def hello_endpoint(endpoint):
     return "endpoint called : {}!\nOutput from Cobol Code : {}".format(endpoint,output)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3030)
+    app.run(host='0.0.0.0', port=3030)
 
