ed5873eb07b71c6c6f73dd49d27ef8aaa6f47909
sandy1480 <43827905+sandy1480@users.noreply.github.com>
2019-02-13 21:00:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def hello_endpoint():
     return "Output from Cobol Code : {}".format(output)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3030)
+    app.run(host='0.0.0.0', port=8090)
