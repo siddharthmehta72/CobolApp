62e5edfd36cfe28a740ef27e1b6ca186fd2c5dbf
sandy1480 <43827905+sandy1480@users.noreply.github.com>
2019-02-15 16:04:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Cobol Merge Application"
 
 
 @app.route('/runcobol', methods=['GET', 'POST'])
@@ -15,7 +15,7 @@ def hello_endpoint():
     file1 = request.args.get('f1')
     file2 = request.args.get('f2')
     file3 = request.args.get('merge')
-    output = subprocess.check_output("/home/TESTMERGE /data/" + file1 + " /data/" + file2 + " /data/" + file3 ,shell=True)
+    output = subprocess.check_output("/home/TESTMERGE /data/" ,shell=True)
     return "Output from Cobol Code : {}".format(output)
 
 if __name__ == '__main__':
