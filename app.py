5f72d78e3cdf5be6699ff5c98267286e16c834bd
Shashi Kumar <shashijangra@gmail.com>
2019-02-18 07:59:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,15 +23,16 @@ def hello():
 
 @app.route('/<version>/runcobol', methods=['POST'])
 def hello_endpoint(version):
-    output = ""
+    output = " "
     try:
         if version == "v1" or version == "latest":
             data = request.data
-            dataDict = json.loads(data)
+            dataDict = json.loads(data.decode("utf-8") )
             file1 = dataDict['f1']
             file2 = dataDict['f2']
             file3 = dataDict['merge']
             cobol_output = subprocess.check_output("/home/TESTMERGE /data/" + file1 + " /data/" + file2 + " /data/" + file3 ,shell=True)
+            output = {"status":"success","output":cobol_output.decode("utf-8")}
         else:
             output = {"status":"error",
                       "error_text": "bad request"}
